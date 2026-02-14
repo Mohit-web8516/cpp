ec93d91fb9e9acbc29336f7e97f8091a6477c14d
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-14 16:59:48 +0530

    CONST VARIABLE

---
+++ const.cpp
@@ -0,0 +1,25 @@
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     const double pi = 3.14; //constant varible means that the value of pi cannot be changed throughout the program
+//     double radius = 10;
+//     double circumference = 2 * pi * radius;
+
+//     cout << circumference << " cm" << endl;
+//     return 0;
+// }
+
+
+////////////////////////////////
+
+
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     const double pi = 3.14;
+//     const int LIGHT_SPEED = 299792458; 
+//     const int WIDTH = 1920;
+//     const int HEIGHT = 1080;
+//     cout << pi << " "<< endl<< LIGHT_SPEED << " " << endl<< WIDTH << " " <<endl<< HEIGHT << endl;
+//     return 0;
+// }
