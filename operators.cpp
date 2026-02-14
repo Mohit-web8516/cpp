0d9ffd3e4e71b5710ac1deb89c2429b66471a6ee
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-14 18:52:21 +0530

    OPERATORS

---
+++ operators.cpp
@@ -0,0 +1,44 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 10;
+//     int y = 5;
+
+//     // Arithmetic Operators
+//     cout << "x + y = " << x + y << endl;
+
+//     cout << "x - y = " << x - y << endl;
+
+//     cout << "x * y = " << x * y << endl; 
+
+//     cout << "x / y = " << x / y << endl; 
+
+//     cout << "x % y = " << x % y << endl; 
+
+//     // Comparison Operators
+//     cout << "x == y: " << (x == y) << endl;
+
+//     cout << "x != y: " << (x != y) << endl;
+
+//     cout << "x > y: " << (x > y) << endl;   
+
+//     cout << "x < y: " << (x < y) << endl; 
+
+//     cout << "x >= y: " << (x >= y) << endl; 
+    
+//     cout << "x <= y: " << (x <= y) << endl;
+    
+//     //logical operators
+//     bool a = true;
+//     bool b = false;
+//     cout << "a && b: " << (a && b) << endl;
+    
+//     cout << "a || b: " << (a || b) << endl;
+    
+//     cout << "!a: " << !a << endl; 
+    
+
+//     return 0;
+// }
+
+////////////////////////////////////
