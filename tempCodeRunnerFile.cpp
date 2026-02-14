92c2f89c0dc3cf66e4e1ffa7b0c4871cc444023f
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-14 19:56:56 +0530

    updated

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,7 @@
-#include<iostream>
-using namespace std;
-int main (){
-    const double pi = 3.14;
-    double radius = 10;
-    double circumference = 2 * pi * radius;
+    // cout << "Floor of num1: " << floor(num1) << endl; // rounds down
 
-    cout << circumference << " cm" << endl;
-}
+    // cout << "Round of num1: " << round(num1) << endl; //rounds to nearest integer
+
+    // cout << "Max of num1 and num2: " << max(num1, num2) << endl; 
+
+    // cout << "Min of num1 and num2: " << min(num1, num2) << endl;
