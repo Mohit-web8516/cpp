2954fec80924b3c5d0c6e9ba71fbfef66953ffe9
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 09:54:49 +0530

    BreakAndContinue

---
+++ conditional_statements/BreakAndContinue.cpp
@@ -0,0 +1,34 @@
+//////////////////////////////
+// Break and Continue Statements = break and continue statements are used to control the flow of loops.
+// The break statement is used to exit a loop prematurely, while the continue statement is used to skip the current iteration of a loop and move on to the next iteration.
+
+
+//continue statement is used to skip the current iteration of a loop and move on to the next iteration. 
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string name;
+//     cout << "ENTER YOUR NAME :";
+//     cin >> name;
+//     for (int i = 0; i < name.length(); i++){
+//         if (name[i] == 'm' || name[i] == 'A')
+//         continue;
+//         cout << name[i] << endl;
+//     }
+
+// }
+
+/////////////////////////////////////////////
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i = 0; i < 10; i++){
+//         if (i == 5)
+//         break;
+//         cout << i << endl;
+//     }
+
+// }
+
+///////////////////////////////////
