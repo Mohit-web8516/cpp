65c9b19524f53ab832d91dc044186442f24088c6
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 10:17:19 +0530

    Random Event Generator

---
+++ conditional_statements/RandomEventGenerator.cpp
@@ -0,0 +1,27 @@
+//random event generator 
+// #include <iostream>
+
+// #include <ctime>
+// using namespace std;
+// int main(){
+//     srand(time(0));
+//     int randNum = (rand() % 5) + 1;
+
+//     switch (randNum){
+//         case 1:
+//             cout << "You got love!" << endl;
+//             break;
+//         case 2:
+//             cout << "You got a hate!" << endl;
+//             break;
+//         case 3:
+//             cout << "You got a surprise!" << endl;
+//             break;
+//         case 4:
+//             cout << "You got a mystery!" << endl;
+//             break;
+//         case 5:
+//             cout << "You got a fortune!" << endl;
+//             break;
+//     }
+// }
