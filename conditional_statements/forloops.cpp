aa351d7ff9f1caf2825e47ac2e5021ec63b49edc
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 09:55:11 +0530

    Fo rLoop

---
+++ conditional_statements/forloops.cpp
@@ -0,0 +1,28 @@
+///////////////////////
+// For loop
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i=0; i<2; i++){
+//         cout << "Happy Birthday!" << endl;
+//     }
+//     return 0;
+// }
+/////////////////////////
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for (int i = 10; i >= 0;i-= 2)
+//     cout << i << endl;
+//     cout << "HELLO" ;
+// }
+
+
+//////////////////////
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 10; i >= 0;i-- )
+    cout << i << endl;
+    cout << "HELLO" ;
+}
