8c1bef4e3f6f8f35fa77c30685bd3fc43d41ba55
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 22:38:10 +0530

    updated

---
+++ Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+include <iostream>
+// using namespace std;
+// int main(){
+
+//     int arr[5] = {1, 2, 3, 4, 5}; // Declaration and initialization of an array
+//     cout << "Elements of the array : " << endl;
+//     for (int i = 0; i < 5; i++) {
+//         cout << arr[i] << "  " << endl; // Accessing elements using index
+//     }
+//     cout << arr[2] << endl; // Accessing the third element (index 2)
+//     cout << endl;
+    
+//     return 0;
+// }
