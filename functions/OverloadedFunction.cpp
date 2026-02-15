16b4a8b863bc7e8085c0ceab4ba115eb52ed8161
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 21:22:04 +0530

    Over loaded function

---
+++ functions/OverloadedFunction.cpp
@@ -0,0 +1,47 @@
+//Overloaded Function = are functions that have the same name but different parameters (either in number, type, or both).
+
+// #include <iostream>
+// using namespace std;
+// void bakePizza();
+// void bakePizza(string topping);
+// void bakePizza(string topping1, string topping2);
+
+// int main(){
+//     bakePizza();
+//     bakePizza("pepperoni", "mushroom");
+//     return 0;
+// }
+// void bakePizza(){
+//     cout << "Baking a pizza..." << endl;
+// }
+// void bakePizza(string topping){
+//     cout << "Here is your  " << topping << " pizza...!!" << endl;
+// }
+// void bakePizza(string topping1, string topping2){
+//     cout << "Here is your  " << topping1 << " and " << topping2 << " pizza...!!" << endl;
+// }
+
+//////////////////////////////////////////////////
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// string dish(string mainIngredient);
+// string dish(string mainIngredient, string sideDish);
+// int main(){
+//     string mainIngredient, sideDish;
+//     cout << "Enter the main ingredient: ";
+//     cin >> mainIngredient;
+//     cout << "Enter the side dish: ";
+//     cin >> sideDish;
+//     string meal1 = dish(mainIngredient);
+//     string meal2 = dish(mainIngredient, sideDish);
+//     cout << meal1 << endl;
+//     cout << meal2 << endl;
+//     return 0;
+// }
+// string dish(string mainIngredient){
+//     return "Here is your " + mainIngredient + " dish!";
+// }
+// string dish(string mainIngredient, string sideDish){
+//     return "Here is your " + mainIngredient + " with " + sideDish + "!";
+// }
