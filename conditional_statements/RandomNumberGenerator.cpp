83ec2726f49e963d888e4fe8146a1c9c0e452d32
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 10:16:54 +0530

    Random Number Generator

---
+++ conditional_statements/RandomNumberGenerator.cpp
@@ -0,0 +1,12 @@
+// Random Number Generator
+// #include <iostream>
+// #include <cstdlib> // for rand() and srand()
+// #include <ctime> // for time()
+// using namespace std;
+// int main(){
+//     //pseudo-random = NOT truly random (but close)
+//     srand(time(0));
+//     int num = (rand() % 100) + 1; // generates a random number between 1 and 100
+//     cout << num;
+//     return 0;
+// }
