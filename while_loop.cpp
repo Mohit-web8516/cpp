42b6638cd4032f2077ff385d3a29c6f31bd522c7
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-15 08:42:16 +0530

    while loop

---
+++ while_loop.cpp
@@ -0,0 +1,67 @@
+//while-loop = a control flow statement that allows code to be executed repeatedly based on a given boolean condition. The while loop can be thought of as a repeating if statement.
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     string name;
+//     while (name != "quit") {
+//         cout << "Enter your name: ";
+//         cin >> name;
+//         cout << "Hello " << name << endl;
+//     }
+
+// } 
+////////////////////////////////
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int number;
+//     cout << "Enter a number: ";
+//     cin >> number;
+//     while (number >= 0) {
+//         cout << "The square of " << number << " is " << number * number << endl;
+//         cout << "Enter a number: ";
+//         cin >> number;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////
+//////////////////////////////////////
+//do-while loop = a control flow statement that executes a block of code at least once, and then repeatedly executes the block, or not, depending on a given boolean condition at the end of the block.
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     string name;
+//     do {
+//         cout << "Enter your name: ";
+//         cin >> name;
+//         cout << "Hello " << name << endl;
+//     } while (name != "quit");
+// }
+
+
+////////////////
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int num;
+//     do {
+//         cout << "ENTER YOUR NUMBER: ";
+//         cin >> num;
+//         cout << "The square of " << num << " is " << num * num << endl;
+//     } while (num >= 0);{
+//             cout << "Negative number entered. Exiting program." << endl;
+//     }
+      
+//     }
