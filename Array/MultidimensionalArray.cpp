1daacfe5a3f0c03bdc5d7764a92afe490794d29f
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-16 22:10:33 +0530

    added new topic

---
+++ Array/MultidimensionalArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   string cars[][3] = {{"Mustang","escape", "F-150"},
+                      {"scorpio","brezza", "bmw"},
+                      {"swift","lambo", "audi"}};
+
+    // cout << cars[0][0] << " ";
+    // cout << cars[0][1] << " ";
+    // cout << cars[0][2] << "\n ";
+    // cout << cars[1][0] << " ";
+    // cout << cars[1][1] << " ";
+    // cout << cars[1][2] << "\n ";
+    // cout << cars[2][0] << " ";
+    // cout << cars[2][1] << " ";
+    // cout << cars[2][2] << "\n ";
+    //////////////////////////////////////////////
+
+    int rows = sizeof(cars)/sizeof(cars[0]);
+    int columns = sizeof(cars[0])/sizeof(cars[0][0]);
+
+    for(int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            cout << cars[i][j] << " ";
+        }
+        cout << '\n';
+    }
+
+       return 0;
+    }
+    
