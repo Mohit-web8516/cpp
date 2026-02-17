6aa694406664dd4a197523a96d8e62cc0ee50452
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-17 21:13:56 +0530

    Null pointer

---
+++ Pointers/NullPointer.cpp
@@ -0,0 +1,33 @@
+/*Null value = a special value that means something has no value.
+when a pointer is holding a null value.
+that pointer is not pointing at anything (null pointer)
+
+nullptr = keyword represents a null pointer literal 
+
+
+
+nullptrs are helpful when determining if an address was successfully assigned to a pointer
+ 
+
+when using pointer ,be careful that your code isn't dereferencing nullptr or pointing to free memory 
+this will cause undefined behavior
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int *pointer = nullptr;
+    int x = 123;
+
+    pointer = &x;
+    
+    if(pointer == nullptr){
+        cout << "address was not assigned !\n";
+    }
+    else{
+        cout << "address was assigned !\n";
+        cout << *pointer;
+    }
+
+    return 0;
+}
