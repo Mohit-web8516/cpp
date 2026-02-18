14363056087d2494fecc50dfe72908e10f588dc6
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-18 21:03:40 +0530

    ConstructorOverloading

---
+++ Oops/ConstructorOverloading.cpp
@@ -0,0 +1,32 @@
+/*
+Overloaded constructors = Multiple constructors w/ same name but different parameters allows for varying arguments when instantiating an object
+*/
+
+#include <iostream>
+using namespace std;
+class Pizza{
+    public:
+    string topping1;
+    string topping2;
+    Pizza(string topping1){
+       this -> topping1 = topping1;
+    }
+    
+    Pizza(string topping1, string topping2){
+       this -> topping1 = topping1;
+       this -> topping2 = topping2;
+    }
+    
+
+};
+int main(){
+
+    Pizza pizza1("pepperoni");
+    Pizza pizza2("mushrooms","peppers");
+
+    cout << pizza1.topping1 << '\n';
+
+    cout << pizza2.topping2 << '\n';
+
+    return 0;
+}
