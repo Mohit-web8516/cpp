45d49ea2f7a3c5beb64537c5f3a7b6f4d55bf543
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-02 15:57:25 +0000

    revise

--- All/revise.cpp
+++ All/revise.cpp
@@ -260,26 +260,63 @@
 
 ///////////////////////////////////
 //////////////////////////////////
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     string str;
+//     cout << "Enter a word: ";
+//     cin >> str;
+
+//     bool isPalindrome = true;
+//     int n = str.length();
+
+//     for(int i = 0; i < n/2; i++) {
+//         if(str[i] != str[n-i-1]) {
+//             isPalindrome = false;
+//             break;
+//         }
+//     }
+
+//     if(isPalindrome) cout << str << " is a Palindrome" << endl;
+//     else cout << str << " is Not a Palindrome" << endl;
+
+//     return 0;
+// }
+
+
+//////////////////////////////
+/////////////////////////////
+
 #include <iostream>
 using namespace std;
 
-int main() {
-    string str;
-    cout << "Enter a word: ";
-    cin >> str;
-
-    bool isPalindrome = true;
-    int n = str.length();
-
-    for(int i = 0; i < n/2; i++) {
-        if(str[i] != str[n-i-1]) {
-            isPalindrome = false;
-            break;
-        }
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n-1;
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        if(arr[mid] == key) return mid;
+        else if(arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
     }
+    return -1;
+}
 
-    if(isPalindrome) cout << str << " is a Palindrome" << endl;
-    else cout << str << " is Not a Palindrome" << endl;
+int main() {
+    int n, key;
+    cout << "Enter size of sorted array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements in sorted order: ";
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int result = binarySearch(arr, n, key);
+    if(result != -1) cout << "Element found at index " << result << endl;
+    else cout << "Element not found" << endl;
 
     return 0;
 }
