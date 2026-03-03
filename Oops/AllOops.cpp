5783934447ed50266dad721a7769870a7dc805f6
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-03 21:11:07 +0530

    oops

---
+++ Oops/AllOops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    int age;
+public:
+    void setData(string n, int a) {
+        name = n; age = a;
+    }
+    void display() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s1;
+    s1.setData("john", 20);
+    s1.display();
+}
