e8b82f3003fe3039728ec75065d25af6ea0059e9
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-03 21:22:12 +0530

    inheritence

--- Oops/allOops.cpp
+++ Oops/allOops.cpp
@@ -25,15 +25,38 @@
 
 
 ///////////////////////////////////////////
+// #include <iostream>
+// using namespace std;
+
+// class Demo {
+// public:
+//     Demo() { cout << "Constructor called\n"; }
+//     ~Demo() { cout << "Destructor called\n"; }
+// };
+
+// int main() {
+//     Demo d;  // Constructor executes
+// } // Destructor executes automatically
+
+
+///////////////////////////////////////////////
+
 #include <iostream>
 using namespace std;
 
-class Demo {
+class Base {
 public:
-    Demo() { cout << "Constructor called\n"; }
-    ~Demo() { cout << "Destructor called\n"; }
+    void greet() { cout << "Hello from Base\n"; }
+};
+
+class Derived : public Base {
+public:
+    void show() { cout << "Hello from Derived\n"; }
 };
 
 int main() {
-    Demo d;  // Constructor executes
-} // Destructor executes automatically
+    Derived d;
+    d.greet();
+    d.show();
+}
+
