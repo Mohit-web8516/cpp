478b570ea802f01c7b86cef4118ff5344bb02da4
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-03 21:20:51 +0530

    constructor

--- Oops/allOops.cpp
+++ Oops/allOops.cpp
@@ -1,20 +1,39 @@
+// #include <iostream>
+// using namespace std;
+
+// class Student {
+//     string name;
+//     int age;
+// public:
+//     void setData(string n, int a) {
+//         name = n; age = a;
+//     }
+//     void display() {
+//         cout << "Name: " << name << ", Age: " << age << endl;
+//     }
+// };
+
+// int main() {
+//     Student s1;
+//     s1.setData("Mohit", 20);
+//     s1.display();
+// }
+
+
+
+
+
+
+///////////////////////////////////////////
 #include <iostream>
 using namespace std;
 
-class Student {
-    string name;
-    int age;
+class Demo {
 public:
-    void setData(string n, int a) {
-        name = n; age = a;
-    }
-    void display() {
-        cout << "Name: " << name << ", Age: " << age << endl;
-    }
+    Demo() { cout << "Constructor called\n"; }
+    ~Demo() { cout << "Destructor called\n"; }
 };
 
 int main() {
-    Student s1;
-    s1.setData("Mohit", 20);
-    s1.display();
-}
+    Demo d;  // Constructor executes
+} // Destructor executes automatically
