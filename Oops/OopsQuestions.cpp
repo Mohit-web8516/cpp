4c193c3e9221f2b008246691539dca1a3c90bb3b
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-04 22:52:18 +0530

    ADDED

--- Oops/OopsQuestions.cpp
+++ Oops/OopsQuestions.cpp
@@ -1,18 +1,34 @@
 //CLASS AND OBJECT BASICS
 
+// #include <iostream>
+// using namespace std;
+
+// class Student {
+//     string name;
+//     int age;
+// public:
+//     void setData(string n, int a) { name = n; age = a; }
+//     void display() { cout << "Name: " << name << ", Age: " << age << endl; }
+// };
+
+// int main() {
+//     Student s1;
+//     s1.setData("john", 20);
+//     s1.display();
+// }
+
+
+//CONSTRUCTOR AND DESTRUCTOR
+
 #include <iostream>
 using namespace std;
 
-class Student {
-    string name;
-    int age;
+class Demo {
 public:
-    void setData(string n, int a) { name = n; age = a; }
-    void display() { cout << "Name: " << name << ", Age: " << age << endl; }
+    Demo() { cout << "Constructor called\n"; }
+    ~Demo() { cout << "Destructor called\n"; }
 };
 
 int main() {
-    Student s1;
-    s1.setData("john", 20);
-    s1.display();
-}
+    Demo d;  // Constructor executes
+} // Destructor executes automatically
