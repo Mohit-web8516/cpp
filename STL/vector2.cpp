9dd097e906f909f8cc68858ef78f03afdddbf053
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 22:26:18 +0530

    VEC2

--- STL/vector2.cpp
+++ STL/vector2.cpp
@@ -1,13 +1,57 @@
 //////////SOME MORE FUNCTIONS//////////////
 
 //CLEAR
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-int main() {
-    vector<int> v = {1, 2, 3, 4};
-    v.clear(); // removes all elements
+// int main() {
+//     vector<int> v = {1, 2, 3, 4};
+//     v.clear(); // removes all elements
 
-    cout << "Size after clear: " << v.size();
-}
+//     cout << "Size after clear: " << v.size();
+// }
+
+
+
+//DELETE
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> v;
+//     if(v.empty()) cout << "Vector is empty";
+//     else cout << "Vector has elements";
+// }
+
+
+
+/////////////////////////////////////
+//INSERT
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> v = {10, 20, 40};
+//     v.insert(v.begin() + 2, 30); // insert 30 at index 2
+
+//     for(int x : v) cout << x << " ";
+// }
+
+
+
+////////////////////////////////
+//ERASE
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> v = {10, 20, 30, 40, 50};
+//     v.erase(v.begin() + 2); // removes element at index 2 (30)
+
+//     for(int x : v) cout << x << " ";
+// }
+
