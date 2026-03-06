c43326affa486801af6ca037b15f8d33a7791f40
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 22:59:15 +0530

    list

---
+++ STL/listCONTAINER.cpp
@@ -0,0 +1,37 @@
+/*
+Common Functions
+Function ||||||||||||||	Description
+push_back(x) =========	Add element at end
+push_front(x)============	Add element at front
+pop_back()	==========Remove last element
+pop_front()==========	Remove first element
+insert(pos, x)============	Insert element at given iterator position
+erase(pos)============	Remove element at given iterator position
+remove(x)==========	Remove all occurrences of value x
+sort()============	Sort elements
+reverse()==========	Reverse order of elements
+clear()=========	Remove all elements
+size()==============	Number of elements
+empty()===========	Check if list is empty
+*/
+
+
+// #include <iostream>
+// #include <list>
+// using namespace std;
+
+// int main() {
+//     list<int> myList;
+    
+//     myList.emplace_back(15);
+//     myList.push_back(10);
+//     myList.push_back(20);
+//     myList.push_front(5);
+
+//     myList.pop_back();
+//     myList.pop_front();
+
+//     cout << "List elements: ";
+//     for(int n : myList) 
+//     cout << n << " ";
+// }
