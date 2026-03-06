d92bf02401b758d890e9abac712bc51f10f94cfb
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 23:32:35 +0530

    stack

---
+++ STL/stack.cpp
@@ -0,0 +1,45 @@
+/*Common Functions
+Function	Description
+push(x)	Add element to top
+pop()	Remove top element
+top()	Access top element
+empty()	Check if stack is empty
+size()	Number of elements*/
+
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top element: " << s.top() << endl; // 30
+
+    s.pop(); // removes 30
+    cout << "New top: " << s.top() << endl;     // 20
+
+    cout << "Size: " << s.size() << endl;       // 2
+}
+
+
+/////////////////////////////////////////
+//////////////////////////////////////////
+// #include <iostream>
+// #include <stack>
+// using namespace std;
+
+// int main() {
+//     stack<string> s;
+
+//     if(s.empty()) cout << "Stack is empty\n";
+
+//     s.push("Mohit");
+//     s.push("Rahul");
+
+//     cout << "Top: " << s.top() << endl; // Rahul
+// }
