0acd9964d1b1ced4aef7360dc4563c5e3c7970f5
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 23:38:37 +0530

    queue

---
+++ STL/queue.cpp
@@ -0,0 +1,28 @@
+/*Common Functions
+Function	Description
+push(x)	Add element at the back
+pop()	Remove element from the front
+front()	Access the first element
+back()	Access the last element
+empty()	Check if queue is empty
+size()	Number of elements*/
+
+
+////////////////////////////////
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    cout << "Front: " << q.front() << endl; // 10
+    cout << "Back: " << q.back() << endl;   // 30
+
+    q.pop(); // removes 10
+    cout << "New Front: " << q.front() << endl; // 20
+}
