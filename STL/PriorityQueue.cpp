c8137bfead73817e37613ad7a3b1c8f98778813e
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 23:45:58 +0530

    priority_queue

--- STL/PriorityQueue.cpp
+++ STL/PriorityQueue.cpp
@@ -6,19 +6,37 @@ empty()	Check if queue is empty
 size()	Number of elements*/
 
 
+// #include <iostream>
+// #include <queue>
+// using namespace std;
+
+// int main() {
+//     priority_queue<int> pq;
+
+//     pq.push(10);
+//     pq.push(30);
+//     pq.push(20);
+
+//     cout << "Top element: " << pq.top() << endl; // 30
+
+//     pq.pop(); // removes 30
+//     cout << "New top: " << pq.top() << endl;     // 20
+// }
+
+
+/////////////////////////////////////////
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
 int main() {
-    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq; // min-heap
 
     pq.push(10);
     pq.push(30);
     pq.push(20);
 
-    cout << "Top element: " << pq.top() << endl; // 30
-
-    pq.pop(); // removes 30
-    cout << "New top: " << pq.top() << endl;     // 20
+    cout << "Top element: " << pq.top() << endl; // 10
 }
+
