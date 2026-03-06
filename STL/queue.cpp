06ebea8ed4e81ba022408f15cf4dd689ac09eaef
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 23:39:52 +0530

    queue

--- STL/queue.cpp
+++ STL/queue.cpp
@@ -8,21 +8,41 @@ empty()	Check if queue is empty
 size()	Number of elements*/
 
 
-////////////////////////////////
+// ////////////////////////////////
+// #include <iostream>
+// #include <queue>
+// using namespace std;
+
+// int main() {
+//     queue<int> q;
+
+//     q.push(10);
+//     q.push(20);
+//     q.push(30);
+
+//     cout << "Front: " << q.front() << endl; // 10
+//     cout << "Back: " << q.back() << endl;   // 30
+
+//     q.pop(); // removes 10
+//     cout << "New Front: " << q.front() << endl; // 20
+// }
+
+
+////////////////////////////////////////////////
+
 #include <iostream>
 #include <queue>
 using namespace std;
 
 int main() {
-    queue<int> q;
+    queue<string> q;
+    q.push("RAM");
+    q.push("Rahul");
+    q.push("Amit");
 
-    q.push(10);
-    q.push(20);
-    q.push(30);
-
-    cout << "Front: " << q.front() << endl; // 10
-    cout << "Back: " << q.back() << endl;   // 30
-
-    q.pop(); // removes 10
-    cout << "New Front: " << q.front() << endl; // 20
+    cout << "Queue elements (FIFO): ";
+    while(!q.empty()) {
+        cout << q.front() << " "; // process front
+        q.pop();                  // remove front
+    }
 }
