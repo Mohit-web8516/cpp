270a2cf17b432a887087eb79ec000019686bc6cf
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-06 23:33:11 +0530

    stack

--- STL/stack.cpp
+++ STL/stack.cpp
@@ -7,39 +7,39 @@ empty()	Check if stack is empty
 size()	Number of elements*/
 
 
-#include <iostream>
-#include <stack>
-using namespace std;
-
-int main() {
-    stack<int> s;
-
-    s.push(10);
-    s.push(20);
-    s.push(30);
-
-    cout << "Top element: " << s.top() << endl; // 30
-
-    s.pop(); // removes 30
-    cout << "New top: " << s.top() << endl;     // 20
-
-    cout << "Size: " << s.size() << endl;       // 2
-}
-
-
-/////////////////////////////////////////
-//////////////////////////////////////////
 // #include <iostream>
 // #include <stack>
 // using namespace std;
 
 // int main() {
-//     stack<string> s;
+//     stack<int> s;
 
-//     if(s.empty()) cout << "Stack is empty\n";
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
 
-//     s.push("Mohit");
-//     s.push("Rahul");
+//     cout << "Top element: " << s.top() << endl; // 30
 
-//     cout << "Top: " << s.top() << endl; // Rahul
+//     s.pop(); // removes 30
+//     cout << "New top: " << s.top() << endl;     // 20
+
+//     cout << "Size: " << s.size() << endl;       // 2
 // }
+
+
+///////////////////////////////////////
+////////////////////////////////////////
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<string> s;
+
+    if(s.empty()) cout << "Stack is empty\n";
+
+    s.push("Mohit");
+    s.push("Rahul");
+
+    cout << "Top: " << s.top() << endl; // Rahul
+}
