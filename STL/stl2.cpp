49d8928065072b927c65bd9cb3b86dc02b286a1e
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-08 15:37:40 +0000

    vector

---
+++ STL/stl2.cpp
@@ -0,0 +1,15 @@
+//vector
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> v = {1, 2, 3};
+//     v.push_back(4);  
+//     v.pop_back();     
+
+//     cout << "Vector elements: ";
+//     for(int x : v) cout << x << " ";
+//     cout << "\nSize: " << v.size();
+// }
