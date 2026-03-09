565bb7b95441e2c40bcc3da6373b82a85bbc81e8
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-09 21:32:04 +0530

    frequency count

--- SomePrograms/programs.cpp
+++ SomePrograms/programs.cpp
@@ -19,25 +19,40 @@
 
 //2. Check Balanced Parentheses using Stack
 
+// #include <iostream>
+// #include <stack>
+// using namespace std;
+
+// bool isBalanced(string s) {
+//     stack<char> st;
+//     for(char c : s) {
+//         if(c=='(' || c=='{' || c=='[') st.push(c);
+//         else {
+//             if(st.empty()) return false;
+//             char top = st.top(); st.pop();
+//             if((c==')' && top!='(') || (c=='}' && top!='{') || (c==']' && top!='['))
+//                 return false;
+//         }
+//     }
+//     return st.empty();
+// }
+
+// int main() {
+//     string s = "{[()]}";
+//     cout << (isBalanced(s) ? "Balanced" : "Not Balanced");
+// }
+
+
+//3. Frequency Count using Map
+
 #include <iostream>
-#include <stack>
+#include <map>
 using namespace std;
 
-bool isBalanced(string s) {
-    stack<char> st;
-    for(char c : s) {
-        if(c=='(' || c=='{' || c=='[') st.push(c);
-        else {
-            if(st.empty()) return false;
-            char top = st.top(); st.pop();
-            if((c==')' && top!='(') || (c=='}' && top!='{') || (c==']' && top!='['))
-                return false;
-        }
-    }
-    return st.empty();
-}
-
 int main() {
-    string s = "{[()]}";
-    cout << (isBalanced(s) ? "Balanced" : "Not Balanced");
+    string str = "programming";
+    map<char,int> freq;
+    for(char c : str) freq[c]++;
+
+    for(auto &p : freq) cout << p.first << " -> " << p.second << "\n";
 }
