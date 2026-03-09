d66292d58b160daa567e5e21a41d51e286654987
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-09 21:42:45 +0530

    factorial

--- SomePrograms/loopsProgram.cpp
+++ SomePrograms/loopsProgram.cpp
@@ -1,9 +1,19 @@
 //1. Sum of First N Natural Numbers (for loop)
 
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n=10, sum=0;
+//     for(int i=1;i<=n;i++) sum+=i;
+//     cout<<"Sum = "<<sum;
+// }
+
+//2. Factorial of a Number (while loop)
+
 #include <iostream>
 using namespace std;
 int main() {
-    int n=10, sum=0;
-    for(int i=1;i<=n;i++) sum+=i;
-    cout<<"Sum = "<<sum;
+    int n=5, fact=1;
+    while(n>0){ fact*=n; n--; }
+    cout<<"Factorial = "<<fact;
 }
