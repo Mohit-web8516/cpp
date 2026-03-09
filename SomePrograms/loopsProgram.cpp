b513e74491be3c4f5d749cf43ef895f229d2bf97
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-09 21:41:53 +0530

    loop

---
+++ SomePrograms/loopsProgram.cpp
@@ -0,0 +1,9 @@
+//1. Sum of First N Natural Numbers (for loop)
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n=10, sum=0;
+    for(int i=1;i<=n;i++) sum+=i;
+    cout<<"Sum = "<<sum;
+}
