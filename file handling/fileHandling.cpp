738b461f0bf2fb7f4a49e0d0431b77c00a3cfd5b
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-10 21:00:29 +0530

    file2

--- file handling/fileHandling.cpp
+++ file handling/fileHandling.cpp
@@ -1,12 +1,31 @@
 //write to a file
 
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// int main() {
+//     ofstream fout("example.txt"); // create/open file
+//     fout << "Hello Mohan, this is file handling in C++!";
+//     fout.close();
+//     cout << "Data written successfully.";
+// }
+
+
+/****************************************** */
+
+//Read from a file
+
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main() {
-    ofstream fout("example.txt"); // create/open file
-    fout << "Hello Mohit, this is file handling in C++!";
-    fout.close();
-    cout << "Data written successfully.";
+    ifstream fin("example.txt");
+    string line;
+    while(getline(fin, line)) {
+        cout << line << "\n";
+    }
+    fin.close();
 }
+
