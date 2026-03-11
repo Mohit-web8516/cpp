42b05a86de2cf79eca8138312a222b1d6c71310e
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-11 20:51:30 +0530

    update

---
+++ quick/let.cpp
@@ -0,0 +1,46 @@
+/*Question
+Write a C++ program to:
+
+Take n integers as input.
+
+Store them in a vector.
+
+Find the maximum, minimum, and average of the numbers.
+
+Print the results.*/
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // for min_element, max_element
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter " << n << " integers:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+   
+    int minVal = *min_element(arr.begin(), arr.end());
+    int maxVal = *max_element(arr.begin(), arr.end());
+
+    // Calculate average
+    double sum = 0;
+    for (int num : arr) {
+        sum += num;
+    }
+    double avg = sum / n;
+
+    
+    cout << "Minimum: " << minVal << endl;
+    cout << "Maximum: " << maxVal << endl;
+    cout << "Average: " << avg << endl;
+
+    return 0;
+}
