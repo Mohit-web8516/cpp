812757e40e612d8772c3be406822a36844820374
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-12 20:31:26 +0530

    added

--- quick/let.cpp
+++ quick/let.cpp
@@ -10,37 +10,37 @@ Find the maximum, minimum, and average of the numbers.
 Print the results.*/
 
 
-#include <iostream>
-#include <vector>
-#include <algorithm> // for min_element, max_element
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <algorithm> // for min_element, max_element
+// using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
 
-    vector<int> arr(n);
-    cout << "Enter " << n << " integers:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     vector<int> arr(n);
+//     cout << "Enter " << n << " integers:\n";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
    
-    int minVal = *min_element(arr.begin(), arr.end());
-    int maxVal = *max_element(arr.begin(), arr.end());
+//     int minVal = *min_element(arr.begin(), arr.end());
+//     int maxVal = *max_element(arr.begin(), arr.end());
 
-    // Calculate average
-    double sum = 0;
-    for (int num : arr) {
-        sum += num;
-    }
-    double avg = sum / n;
+//     // Calculate average
+//     double sum = 0;
+//     for (int num : arr) {
+//         sum += num;
+//     }
+//     double avg = sum / n;
 
     
-    cout << "Minimum: " << minVal << endl;
-    cout << "Maximum: " << maxVal << endl;
-    cout << "Average: " << avg << endl;
+//     cout << "Minimum: " << minVal << endl;
+//     cout << "Maximum: " << maxVal << endl;
+//     cout << "Average: " << avg << endl;
 
-    return 0;
-}
+//     return 0;
+// }
