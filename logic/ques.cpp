13ca9b7477dc1c8854157a7f6b3b67a3d281cdbe
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-13 19:55:07 +0530

    logic

--- logic/ques.cpp
+++ logic/ques.cpp
@@ -1,23 +1,45 @@
 //question based on while loop
 //Find the largest digit in the given number. 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    int num, digit, largest = 0;
-    cout << "Enter a number: ";
-    cin >> num;
+// int main()
+// {
+//     int num, digit, largest = 0;
+//     cout << "Enter a number: ";
+//     cin >> num;
 
-    while(num != 0)
-    {
-        digit = num % 10;
-        if(digit > largest)
-        {
-            largest = digit;
-        }
-        num = num / 10;
-    }
-    cout << "Largest digit = " << largest;
-    return 0;
-}
+//     while(num != 0)
+//     {
+//         digit = num % 10;
+//         if(digit > largest)
+//         {
+//             largest = digit;
+//         }
+//         num = num / 10;
+//     }
+//     cout << "Largest digit = " << largest;
+//     return 0;
+// }
+
+//////////////////////////////////
+//print all factor of a given num
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num;
+//     cout << "Enter number: ";
+//     cin >> num;
+//     cout << "Factors are: ";
+//     for(int i = 1; i <= num; i++)
+//     {
+//         if(num % i == 0)
+//         {
+//             cout << i << " ";
+//         }
+//     }
+//     return 0;
+// }
