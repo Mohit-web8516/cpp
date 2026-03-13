16dbe65695dbb496a89af7053242a9bc28bb1339
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-13 19:58:00 +0530

    sum of factor

--- logic/ques.cpp
+++ logic/ques.cpp
@@ -43,3 +43,26 @@
 //     }
 //     return 0;
 // }
+
+
+//////////////////////////////////////
+
+//sum of all factors of a number
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num, sum = 0;
+//     cout << "Enter number: ";
+//     cin >> num;
+//     for(int i = 1; i <= num; i++)
+//     {
+//         if(num % i == 0)
+//         {
+//             sum += i;
+//         }
+//     }
+//     cout << "Sum of factors = " << sum;
+//     return 0;
+// }
