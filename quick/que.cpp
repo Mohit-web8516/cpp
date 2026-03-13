ac216d6d43874d8b6875fdbff2b7918ef5b2b8e8
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-13 19:48:04 +0530

    update

--- quick/que.cpp
+++ quick/que.cpp
@@ -17,22 +17,34 @@
 
 //check prime number
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n, count = 0;
+
+//     cout << "Enter number: ";
+//     cin >> n;
+
+//     for(int i = 1; i <= n; i++) {
+//         if(n % i == 0)
+//             count++;
+//     }
+
+//     if(count == 2)
+//         cout << "Prime Number";
+//     else
+//         cout << "Not Prime";
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n, count = 0;
-
-    cout << "Enter number: ";
-    cin >> n;
-
-    for(int i = 1; i <= n; i++) {
-        if(n % i == 0)
-            count++;
+    for(int i=1; i<=100; i++) {
+        if(i % 2 != 0) {   
+            cout << i << endl;
+        }
     }
-
-    if(count == 2)
-        cout << "Prime Number";
-    else
-        cout << "Not Prime";
+    return 0;
 }
