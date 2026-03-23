d58c3e20e1e0dbba399de586cbb2d65ccbd4a150
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-23 20:57:31 +0530

    reverse

--- logic/ques.cpp
+++ logic/ques.cpp
@@ -69,14 +69,36 @@
 
 
 /////////////////////////////
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     // Traversal
+//     for(int i = 0; i < 5; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+//////////////////////////////////////////
+// 1. Reverse an Array
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    // Traversal
-    for(int i = 0; i < 5; i++) {
-        cout << arr[i] << " ";
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int start = 0, end = n-1;
+    while(start < end) {
+        swap(arr[start], arr[end]);
+        start++; end--;
     }
+
+    cout << "Reversed Array: ";
+    for(int i=0; i<n; i++) cout << arr[i] << " ";
     return 0;
 }
+
