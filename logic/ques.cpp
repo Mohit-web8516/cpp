2726ea84286e62c5b5bde8f0271139c2054e9f93
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-24 22:16:50 +0530

    KADANE'S

--- logic/ques.cpp
+++ logic/ques.cpp
@@ -234,55 +234,82 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int maxProductPath(vector<vector<int>>& grid) {
+//         const long long MOD = 1e9 + 7;
+//         int m = grid.size(), n = grid[0].size();
+
+//         vector<vector<long long>> maxProd(m, vector<long long>(n, 0));
+//         vector<vector<long long>> minProd(m, vector<long long>(n, 0));
+
+//         maxProd[0][0] = minProd[0][0] = grid[0][0];
+
+//         // First row
+//         for(int j=1; j<n; j++) {
+//             maxProd[0][j] = maxProd[0][j-1] * grid[0][j];
+//             minProd[0][j] = minProd[0][j-1] * grid[0][j];
+//         }
+
+//         // First column
+//         for(int i=1; i<m; i++) {
+//             maxProd[i][0] = maxProd[i-1][0] * grid[i][0];
+//             minProd[i][0] = minProd[i-1][0] * grid[i][0];
+//         }
+
+//         // Rest of the matrix
+//         for(int i=1; i<m; i++) {
+//             for(int j=1; j<n; j++) {
+//                 long long val = grid[i][j];
+//                 long long maxFromTop = max(maxProd[i-1][j]*val, minProd[i-1][j]*val);
+//                 long long maxFromLeft = max(maxProd[i][j-1]*val, minProd[i][j-1]*val);
+//                 long long minFromTop = min(maxProd[i-1][j]*val, minProd[i-1][j]*val);
+//                 long long minFromLeft = min(maxProd[i][j-1]*val, minProd[i][j-1]*val);
+
+//                 maxProd[i][j] = max(maxFromTop, maxFromLeft);
+//                 minProd[i][j] = min(minFromTop, minFromLeft);
+//             }
+//         }
+
+//         long long result = maxProd[m-1][n-1];
+//         if(result < 0) return -1;
+//         return result % MOD;
+//     }
+// };
+
+// int main() {
+//     Solution sol;
+//     vector<vector<int>> grid = {{1,-2,1},{1,-2,1},{3,-4,1}};
+//     cout << sol.maxProductPath(grid) << endl; // Expected output: 8
+//     return 0;
+// }
+
+
+
+
+
+//////////////////////////////////////////////
+
+
+///Program 2: Kadane’s Algorithm (Maximum Subarray Sum)
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    int maxProductPath(vector<vector<int>>& grid) {
-        const long long MOD = 1e9 + 7;
-        int m = grid.size(), n = grid[0].size();
-
-        vector<vector<long long>> maxProd(m, vector<long long>(n, 0));
-        vector<vector<long long>> minProd(m, vector<long long>(n, 0));
-
-        maxProd[0][0] = minProd[0][0] = grid[0][0];
-
-        // First row
-        for(int j=1; j<n; j++) {
-            maxProd[0][j] = maxProd[0][j-1] * grid[0][j];
-            minProd[0][j] = minProd[0][j-1] * grid[0][j];
-        }
-
-        // First column
-        for(int i=1; i<m; i++) {
-            maxProd[i][0] = maxProd[i-1][0] * grid[i][0];
-            minProd[i][0] = minProd[i-1][0] * grid[i][0];
-        }
-
-        // Rest of the matrix
-        for(int i=1; i<m; i++) {
-            for(int j=1; j<n; j++) {
-                long long val = grid[i][j];
-                long long maxFromTop = max(maxProd[i-1][j]*val, minProd[i-1][j]*val);
-                long long maxFromLeft = max(maxProd[i][j-1]*val, minProd[i][j-1]*val);
-                long long minFromTop = min(maxProd[i-1][j]*val, minProd[i-1][j]*val);
-                long long minFromLeft = min(maxProd[i][j-1]*val, minProd[i][j-1]*val);
-
-                maxProd[i][j] = max(maxFromTop, maxFromLeft);
-                minProd[i][j] = min(minFromTop, minFromLeft);
-            }
-        }
-
-        long long result = maxProd[m-1][n-1];
-        if(result < 0) return -1;
-        return result % MOD;
-    }
-};
-
 int main() {
-    Solution sol;
-    vector<vector<int>> grid = {{1,-2,1},{1,-2,1},{3,-4,1}};
-    cout << sol.maxProductPath(grid) << endl; // Expected output: 8
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = arr.size();
+
+    int maxSum = arr[0], currSum = arr[0];
+    for(int i=1; i<n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+
+    cout << "Maximum Subarray Sum: " << maxSum << endl; // Expected output: 6
     return 0;
 }
